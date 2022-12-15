537d786e2047fb8ca2090d7d34b6efeda22a16a7
fadi boubakri <fadiiboubakii@gmail.com>
2022-12-15 14:14:15 +0100

    config updated

--- utils/Cloudinary.js
+++ utils/Cloudinary.js
@@ -1,5 +1,5 @@
 const cloudinary = require("cloudinary").v2;
-require('dotenv').config({path: './config/.env'});
+require('dotenv').config('./env');
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
